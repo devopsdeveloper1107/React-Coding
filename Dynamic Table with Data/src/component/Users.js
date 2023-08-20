1ad1b96a89145982d62410033b998123c692d29f
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-08-20 15:13:59 +0530

    Add files via upload

--- Dynamic Table with Data/src/component/Users.js
+++ Dynamic Table with Data/src/component/Users.js
@@ -1,1 +1,23 @@
+import React from "react";
 
+export default function Users({ usersData}){
+    //console.log(usersData);
+    return(
+        <React.Fragment>
+            <>
+            {
+             usersData.map((uData, index)=>(
+                <tr key={uData.id}>
+                    <td>{uData.id} </td>
+                    <td>{uData.username} </td>
+                    <td>{uData.email} </td>
+                    <td>{uData.address.city} </td>
+
+                </tr>
+             ))
+            }
+            
+            </>
+        </React.Fragment>
+    );
+}
